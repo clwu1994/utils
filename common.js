3edf343ef2957dc371d2fba8bab84c5b8e409f3e
clwu1994 <1445860318@qq.com>
2019-08-25 22:22:00 +0800

    feat: 新增service目录结构

---
+++ common.js
@@ -0,0 +1,13 @@
+const fileNameRE = /^\.\/([^\s]+)\.js$/;
+export function importAll(modulesContext) {
+  return modulesContext.keys().map(modulesPath => {
+    const moduleName = modulesPath.match(fileNameRE);
+    return {
+      moduleName,
+      camelModuleName: moduleName[1].replace(/-(\w)/g, (_, c) =>
+        c ? c.toUpperCase() : ""
+      ),
+      module: modulesContext(modulesPath).default
+    };
+  });
+}
